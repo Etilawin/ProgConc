d9e43883f574017c4178cbcd69eb175e4370a870
Kim Vallée <kim.vallee@protonmail.com>
2019-12-13 21:05:39 +0100

    Added TME11

---
+++ TME11/src/semaphore/Kong.java
@@ -0,0 +1,27 @@
+package semaphore;
+
+public class Kong extends Babouin {
+
+    public Kong(Corde laCorde) {
+        this.laCorde = laCorde;
+    }
+
+    @Override
+    public String toString() {
+        return "semaphore.Kong";
+    }
+
+    @Override
+    public void run() {
+        try {
+            batifoler();
+            laCorde.accederKong();
+            System.out.println(this.toString() + " a pris la corde.");
+            traverser();
+            System.out.println(this.toString() + " est arrivé.");
+            laCorde.lacherKong();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
